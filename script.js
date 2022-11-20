3b4f86689662349d48eadadea3e106b8edf5ed05
Alex <alexthegreitsas@gmail.com>
2022-11-20 04:21:09 +0200

    add fully functional function to make book cards, console only for now

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function createBookElement(title,pages)
+function createBookElement(title,pages,author,hasReadBook)
 {
   //create a new div called book with the class of book
   const bookdiv = document.createElement('div');
@@ -31,6 +31,12 @@ function createBookElement(title,pages)
   titleContent.classList.add('titleContent');
   titleContent.innerText = title;
   bookTitle.appendChild(titleContent);
+  //make a button called deleteBtn with a class of deleteBtn and innertext X
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add('deleteBtn');
+  deleteBtn.innerText = 'X';
+  bookHeader.appendChild(deleteBtn);
+
   //append bookHeader to book for it to become a child of book
   bookdiv.appendChild(bookHeader);
 
@@ -45,7 +51,7 @@ function createBookElement(title,pages)
   //the first div will be named bookPages with the class bookPages, it will take as an input the pages from the function, must be a number if not default to 1
   const bookPages = document.createElement('div');
   bookPages.classList.add('bookPages');
-  bookPages.innerText = pages;
+  bookPages.innerText = '-Pages:' + pages;
   bookBody.appendChild(bookPages);
   //the second div will be named haveYouReadDiv which will have the class of haveYouReadThisBook
   const haveYouReadDiv = document.createElement('div');
@@ -56,11 +62,67 @@ function createBookElement(title,pages)
   radioForm.classList.add('radioForm');
   //the first div will be holding out the yes input, and the second will be holding the no input
   const div1 = document.createElement('div');
+  radioForm.appendChild(div1);
+  const div2 = document.createElement('div');
+  radioForm.appendChild(div2);
   //each div will have a input of type radio, name yes/noBtn, id of yes/no, name of Option, and a value of yes/no
-  //each div will have a br after the input, then a label for each input with a class of yes/no
+  const radioBtnYes = document.createElement('input');
+  radioBtnYes.setAttribute('type', 'radio');
+  radioBtnYes.classList.add('yesBtn');
+  radioBtnYes.id = 'yes';
+  radioBtnYes.setAttribute('name', 'Option');
+  radioBtnYes.setAttribute('value', 'yes');
+
+  const radioBtnNo = document.createElement('input');
+  radioBtnNo.setAttribute('type', 'radio');
+  radioBtnNo.classList.add('noBtn');
+  radioBtnNo.id = 'no';
+  radioBtnNo.setAttribute('name', 'Option');
+  radioBtnNo.setAttribute('value', 'no');
+
+  //we then append each button to their respective div
+  div1.appendChild(radioBtnYes);
+  div2.appendChild(radioBtnNo);
+  //each div will have a br after the input and append them
+  const br1 = document.createElement('br');
+  const br2 = document.createElement('br');
+  div1.appendChild(br1);
+  div2.appendChild(br2);
+  //then a label for each input with a class of yes/no and append them after the br
+  const labelYes = document.createElement('label');
+  labelYes.htmlFor = 'yes';
+  labelYes.classList.add('yes');
+  labelYes.innerText = "yes";
+  div1.appendChild(labelYes);
+  const labelNo = document.createElement('label');
+  labelNo.htmlFor = 'no';
+  labelNo.classList.add('no');
+  labelNo.innerText = "no";
+  div2.appendChild(labelNo);
   //depending on the value of haveYouReadIt, make either the yes or no radio default
+  if(hasReadBook === true)
+  {
+    radioBtnYes.checked = true;
+  }
+  else
+  {
+    radioBtnNo.checked = true;
+  }
+  //at the end append radioForm to bookbody
+  bookBody.appendChild(radioForm);
 
   //then create a new div called bookFooter, with the class bookFooter which will be a child of book
+  const bookFooter = document.createElement('div');
+  bookFooter.classList.add('bookFooter');
+  bookFooter.classList.add('bookComponent');
+  bookdiv.appendChild(bookFooter);
   //it will have a div called authorName with a class of Authorname that will take as input the author from the function
+  const authorName = document.createElement('div');
+  authorName.classList.add('authorName')
+  authorName.innerText = 'By:' + author;
+  bookFooter.appendChild(authorName);
+
+
+
   document.body.appendChild(bookdiv);
 }
